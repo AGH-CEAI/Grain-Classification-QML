d952d5482ef3153817e3c33fb2f5112fd7e3f8ea
sdziura <sabek12@gmail.com>
2025-09-19 16:18:18 +0200

    Print initial results to file

--- main.py
+++ main.py
@@ -14,13 +14,21 @@ def main():
     X, y = preprocessing.preprocess_data(df)
     X, y = preprocessing.pd_to_numpy_X_y(X, y)
 
-    model = models.get_knn_model()
-    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
+    # model = models.get_knn_model()
+    # cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
     # precision_macro = make_scorer(precision_score, average="macro")
-    y_pred = cross_val_predict(model, X, y, cv=cv)
+    # y_pred = cross_val_predict(model, X, y, cv=cv)
 
-    with open("results/test_file.txt", "x") as f:
-        evaluation.print_report_to_file(f, "test", y, y_pred)
+    with open("results/initial_classical_results.txt", "x") as f:
+        for model_name, model_fun in models.model_functions.items():
+            model = model_fun()
+            cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
+            y_pred = cross_val_predict(model, X, y, cv=cv)
+            evaluation.print_report_to_file(f, model_name, y, y_pred)
+            evaluation.print_conf_matrix_to_file(f, model_name, y, y_pred)
+            f.write(
+                "\n\n******************************************************************************\n"
+            )
 
 
 if __name__ == "__main__":
