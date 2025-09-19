d6e49a4c46b5e761b68930c2bb8ab764be8e246d
sdziura <sabek12@gmail.com>
2025-09-19 15:56:18 +0200

    Add functions for printing results to file

--- main.py
+++ main.py
@@ -6,22 +6,21 @@ from sklearn.metrics import (
 from sklearn.model_selection import StratifiedKFold
 import preprocessing
 import models
+import evaluation
 
 
 def main():
     df = preprocessing.get_excel_data()
     X, y = preprocessing.preprocess_data(df)
     X, y = preprocessing.pd_to_numpy_X_y(X, y)
+
     model = models.get_knn_model()
     cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
     # precision_macro = make_scorer(precision_score, average="macro")
     y_pred = cross_val_predict(model, X, y, cv=cv)
 
-    # Per-class precision/recall/F1
-    print(classification_report(y, y_pred))
-
-    # Confusion matrix
-    print(confusion_matrix(y, y_pred))
+    with open("results/test_file.txt", "x") as f:
+        evaluation.print_report_to_file(f, "test", y, y_pred)
 
 
 if __name__ == "__main__":
