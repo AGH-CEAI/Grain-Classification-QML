e5c7b9e4f4b64d2c35243b406bdec2550f01155e
sdziura <sabek12@gmail.com>
2025-10-03 14:23:06 +0200

    Add eperiment.py module

---
+++ experiments.py
@@ -0,0 +1,23 @@
+from sklearn.calibration import cross_val_predict
+from sklearn.model_selection import StratifiedKFold
+import evaluation
+import models
+import preprocessing
+
+
+def exp_run_all_class_models(file_name: str, seed: int):
+    df = preprocessing.get_excel_data()
+    X, y = preprocessing.preprocess_data(df)
+    X, y = preprocessing.pd_to_numpy_X_y(X, y)
+
+    with open(f"results/{file_name}.txt", "x") as f:
+        f.write(f"\nSEED: {seed}\n")
+        for model_name, model_fun in models.model_functions.items():
+            model = model_fun()
+            cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
+            y_pred = cross_val_predict(model, X, y, cv=cv)
+            evaluation.print_report_to_file(f, model_name, y, y_pred)
+            evaluation.print_conf_matrix_to_file(f, model_name, y, y_pred)
+            f.write(
+                "\n\n******************************************************************************\n"
+            )
--- main.py
+++ main.py
@@ -4,31 +4,18 @@ from sklearn.metrics import (
     confusion_matrix,
 )
 from sklearn.model_selection import StratifiedKFold
+from experiments import exp_run_all_class_models
 import preprocessing
 import models
 import evaluation
 
 
 def main():
-    df = preprocessing.get_excel_data()
-    X, y = preprocessing.preprocess_data(df)
-    X, y = preprocessing.pd_to_numpy_X_y(X, y)
-
+    exp_run_all_class_models("initial_classical_results_42_mlp_new", 42)
     # model = models.get_knn_model()
     # cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
     # precision_macro = make_scorer(precision_score, average="macro")
     # y_pred = cross_val_predict(model, X, y, cv=cv)
-
-    with open("results/initial_classical_results.txt", "x") as f:
-        for model_name, model_fun in models.model_functions.items():
-            model = model_fun()
-            cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
-            y_pred = cross_val_predict(model, X, y, cv=cv)
-            evaluation.print_report_to_file(f, model_name, y, y_pred)
-            evaluation.print_conf_matrix_to_file(f, model_name, y, y_pred)
-            f.write(
-                "\n\n******************************************************************************\n"
-            )
 
 
 if __name__ == "__main__":
