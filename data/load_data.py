6d9e4c33afbd2b67b324ee7150f747cbac424aca
sdziura <sabek12@gmail.com>
2025-11-19 18:35:43 +0100

    Correct type annotation

--- data/load_data.py
+++ data/load_data.py
@@ -13,10 +13,10 @@ def get_excel_data() -> pd.DataFrame:
     return pd.read_excel(EXCEL_DATA_LOCATION)
 
 
-def get_image_data() -> Tuple[List[str], List[Image.Image]]:
+def get_image_data() -> Tuple[List[str], List[np.ndarray]]:
     # Get the list of all files and directories
     dir_list = os.listdir(IMAGE_DATA_LOCATION)
-    imgs = []
+    imgs: List[np.ndarray] = []
     for filename in dir_list:
         with Image.open(os.path.join(IMAGE_DATA_LOCATION, filename)) as img:
             imgs.append(np.array(img))
