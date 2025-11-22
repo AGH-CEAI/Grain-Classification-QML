69e7f1775c76351a3317712041aafb3de62ba192
sdziura <sabek12@gmail.com>
2025-11-22 20:56:25 +0100

    Add plotting images function

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,5 @@
 from typing import TextIO
+from matplotlib import pyplot as plt
 import numpy as np
 from sklearn.metrics import (
     classification_report,
@@ -20,3 +21,18 @@ def print_conf_matrix_to_file(
     file.write(f"\nConfusion matrix for {model_name}:\n")
     report: str = str(confusion_matrix(y, y_pred))
     file.write(report)
+
+
+def show_images(images, cols=5, figsize=(12, 10)):
+    n = len(images)
+    rows = (n // cols) + 1
+
+    plt.figure(figsize=figsize)
+
+    for i, img in enumerate(images):
+        plt.subplot(rows, cols, i + 1)
+        plt.imshow(img, cmap="gray" if img.mode == "L" else None)
+        plt.axis("off")
+
+    plt.tight_layout()
+    plt.show()
